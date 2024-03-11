930864f10a116fa4402fa568b6ec0a124739e6ef
Hikari <158005693+webHikari@users.noreply.github.com>
2024-03-11 22:28:31 +0300

    +datepick

--- client/src/components/Verification/VerificationForm.jsx
+++ client/src/components/Verification/VerificationForm.jsx
@@ -1,8 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Verification.module.css';
-import { IoCloseSharp } from 'react-icons/io5';
 
 const VerificationForm = ({ showVerificationForm, closeVerificationForm }) => {
+    const [documentDay, setDocumentDay] = useState('');
+    const [documentMonth, setDocumentMonth] = useState('');
+    const [documentYear, setDocumentYear] = useState('');
+
+    const handleDayInputChange = (e) => {
+        const inputValue = e.target.value;
+        const numericValue = inputValue.replace(/[^0-9]/g, '');
+        const limitedValue = numericValue.slice(0, 2);
+        const finalValue = limitedValue <= 12 ? limitedValue : '12';
+        setDocumentDay(finalValue);
+    };
+    const handleMonthInputChange = (e) => {
+        const inputValue = e.target.value;
+        const numericValue = inputValue.replace(/[^0-9]/g, '');
+        const limitedValue = numericValue.slice(0, 2);
+        const finalValue = limitedValue <= 31 ? limitedValue : '31';
+        setDocumentMonth(finalValue);
+    };
+    const handleYearInputChange = (e) => {
+        const inputValue = e.target.value;
+        const numericValue = inputValue.replace(/[^0-9]/g, '');
+        const limitedValue = numericValue.slice(0, 4);
+        const finalValue = limitedValue <= 2006 ? limitedValue : '2006';
+        setDocumentYear(finalValue);
+    };
+
     const sendVerificationData = (e) => {
         e.preventDefault();
         console.log('submitted');
@@ -16,11 +41,13 @@ const VerificationForm = ({ showVerificationForm, closeVerificationForm }) => {
         >
             <form onSubmit={(e) => sendVerificationData(e)}>
                 <div>
+                    <br />
                     <div>1. Insert your data</div>
                     <div>2. Verification pending</div>
                     <div>3. You're verified</div>
                 </div>
                 <div>
+                    <br />
                     <label htmlFor="verificationName">
                         <p>Name</p>
                         <input
@@ -45,6 +72,38 @@ const VerificationForm = ({ showVerificationForm, closeVerificationForm }) => {
                             placeholder="Last name"
                         />
                     </label>
+                    <br />
+                    <label htmlFor="documentDay">
+                        <p>Day</p>
+                        <input
+                            type="text"
+                            value={documentDay}
+                            onChange={handleDayInputChange}
+                            placeholder="Day"
+                            name="documentDay"
+                        />
+                    </label>
+                    <label htmlFor="documentMonth">
+                        <p>Month</p>
+                        <input
+                            type="text"
+                            value={documentMonth}
+                            onChange={handleMonthInputChange}
+                            placeholder="Birth month"
+                            name="documentMonth"
+                        />
+                    </label>
+                    <label htmlFor="documentYear">
+                        <p>Year</p>
+                        <input
+                            type="text"
+                            value={documentYear}
+                            onChange={handleYearInputChange}
+                            placeholder="Birth year"
+                            name="documentYear"
+                        />
+                    </label>
+                    <br />
                     <button type="submit">Submit</button>
                 </div>
             </form>
